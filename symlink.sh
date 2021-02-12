9588859aebef5fb096527210bf4c43a509d8dab5
kat <kittencalledkat@gmail.com>
2021-02-12 12:49:11 -0800

    added symlink script

---
+++ symlink.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# This script will symlink all of the dotfiles in this directory to the $HOME
+
+ln -s ~/.dotfiles/dotfiles/vimrc ~/.vimrc
+
+ln -s ~/.dotfiles/dotfiles/slate.js ~/.slate.js
+
+ln -s ~/.dotfiles/dotfiles/zshrc ~/.zshrc
+
+ln -s ~/.dotfiles/dotfiles/dark-meadow.vim ~/.vim/colors/dark-meadow.vim
